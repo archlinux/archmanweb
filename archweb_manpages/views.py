69577f6b6b83bccc4d5cd5cb02b22c76af19b06c
Jakub Klinkovský <j.l.k@gmx.com>
2017-08-27 00:34:41 +0200

    add symlinks to per-package listings

--- archweb_manpages/views.py
+++ archweb_manpages/views.py
@@ -21,27 +21,41 @@ def index(request):
     }
     return render(request, "index.html", context)
 
+def paginate(request, url_param, query, limit):
+    paginator = Paginator(query, limit)
+    page = request.GET.get(url_param)
+    try:
+        query = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver the first page.
+        query = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range, deliver the last page.
+        query = paginator.page(paginator.num_pages)
+    return query
+
 def listing(request, *, repo=None, pkgname=None):
     sorting = request.GET.get("sorting", "alphabetical")
     lang = request.GET.get("lang")
     section = request.GET.get("section")
 
     if sorting == "alphabetical":
-        man_pages = ManPage.objects.order_by("name", "lang", "section")
+        sorting_columns = ("name", "lang", "section")
     elif sorting == "-alphabetical":
-        man_pages = ManPage.objects.order_by("-name", "-lang", "-section")
+        sorting_columns = ("-name", "-lang", "-section")
     elif sorting == "section":
-        man_pages = ManPage.objects.order_by("section", "name", "lang")
+        sorting_columns = ("section", "name", "lang")
     elif sorting == "-section":
-        man_pages = ManPage.objects.order_by("-section", "-name", "-lang")
+        sorting_columns = ("-section", "-name", "-lang")
     elif sorting == "lang":
-        man_pages = ManPage.objects.order_by("lang", "name", "section")
+        sorting_columns = ("lang", "name", "section")
     elif sorting == "-lang":
-        man_pages = ManPage.objects.order_by("-lang", "-name", "-section")
+        sorting_columns = ("-lang", "-name", "-section")
     else:
         raise HttpResponse("Unknown sorting parameter: {}".format(sorting), status=400)
 
     db_pkg = None
+    man_pages = ManPage.objects.order_by( *sorting_columns )
 
     if pkgname:
         # check that such package exists
@@ -67,20 +81,32 @@ def listing(request, *, repo=None, pkgname=None):
     if section:
         man_pages = man_pages.filter(section=section)
 
-    paginator = Paginator(man_pages, 500)
-    page = request.GET.get("page")
-    try:
-        man_pages = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver the first page.
-        man_pages = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range, deliver the last page.
-        man_pages = paginator.page(paginator.num_pages)
+    # list of symbolic links in a package
+    if pkgname:
+        symlinks_sorting_columns = []
+        for c in sorting_columns:
+            if "name" in c:
+                symlinks_sorting_columns.append(c.replace("name", "from_name"))
+            elif "section" in c:
+                symlinks_sorting_columns.append(c.replace("section", "from_section"))
+        symlinks = SymbolicLink.objects.order_by( *symlinks_sorting_columns ).filter(package__name=pkgname)
+        symlinks_count = SymbolicLink.objects.filter(package__name=pkgname).count()
+    else:
+        symlinks = []
+        symlinks_count = 0
+
+    # template rendering time is dominated by the number of links, symlinks have 2 links per row
+    if symlinks_count > 125:
+        man_pages = paginate(request, "page", man_pages, 250)
+        symlinks = paginate(request, "page_symlinks", symlinks, 125)
+    else:
+        man_pages = paginate(request, "page", man_pages, 500)
+        symlinks = paginate(request, "page_symlinks", symlinks, 500)
 
     context = {
         "pkg": db_pkg,
         "man_pages": man_pages,
+        "symlinks": symlinks,
     }
     return render(request, "listing.html", context)
 
