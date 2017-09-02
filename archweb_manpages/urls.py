f0cb8fd9796b66a7c868dfa06a0df82e4ff382d1
Jakub Klinkovský <j.l.k@gmx.com>
2017-09-03 00:45:06 +0200

    fix order of url patterns

--- archweb_manpages/urls.py
+++ archweb_manpages/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<template_name>[a-z]+)', views.simple_view, name='simple_view'),
     url(r'^listing/((?P<repo>[A-Za-z0-9@._+\-]+)/)??((?P<pkgname>[A-Za-z0-9@._+\-]+)/)?$', views.listing, name='listing'),
     url(r'^man/'
         r'((?P<repo>[A-Za-z0-9@._+\-]+)/)??'
@@ -12,4 +11,5 @@ urlpatterns = [
         r'(?P<name_section_lang>[A-Za-z0-9@._+\-:\[\]]+?)'
         r'(\.(?P<url_output_type>html|txt|raw))?$',
         views.man_page, name='man_page'),
+    url(r'^(?P<template_name>[a-z]+)', views.simple_view, name='simple_view'),
 ]
