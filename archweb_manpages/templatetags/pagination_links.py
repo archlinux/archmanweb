701eb84bb08010fc3f1aa2de8ba58c46b414edf1
Jakub Klinkovský <j.l.k@gmx.com>
2018-08-12 14:29:20 +0200

    fix pagination_links template tag
    Probably due to migration to django 2.1...
    
    Fixes #6

--- archweb_manpages/templatetags/pagination_links.py
+++ archweb_manpages/templatetags/pagination_links.py
@@ -10,13 +10,13 @@ def pagination_links(request, paginator, query_string_param):
     # link to the first page
     if paginator.number > 1:
         q = request.GET.copy()
-        q[query_string_param] = 1
+        q[query_string_param] = "1"
         html += format_html('<a href="?{}">first</a>\n', q.urlencode())
 
     # link to the previous page
     if paginator.has_previous():
         q = request.GET.copy()
-        q[query_string_param] = paginator.previous_page_number()
+        q[query_string_param] = str(paginator.previous_page_number())
         html += format_html('<a href="?{}">previous</a>\n', q.urlencode())
 
     # current/total page numbers
@@ -25,13 +25,13 @@ def pagination_links(request, paginator, query_string_param):
     # link to the next page
     if paginator.has_next():
         q = request.GET.copy()
-        q[query_string_param] = paginator.next_page_number()
+        q[query_string_param] = str(paginator.next_page_number())
         html += format_html('<a href="?{}">next</a>\n', q.urlencode())
 
     # link to the last page
     if paginator.number < paginator.paginator.num_pages:
         q = request.GET.copy()
-        q[query_string_param] = paginator.paginator.num_pages
+        q[query_string_param] = str(paginator.paginator.num_pages)
         html += format_html('<a href="?{}">last</a>\n', q.urlencode())
 
     html += '</nav>\n'
