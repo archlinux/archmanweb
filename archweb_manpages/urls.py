7a6b618caee4ae957a896837cdab6c004840ef19
Jakub Klinkovský <j.l.k@gmx.com>
2020-09-23 15:36:11 +0200

    urls: use re_path instead of the deprecated alias django.conf.urls.url

--- archweb_manpages/urls.py
+++ archweb_manpages/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^listing(/(?P<repo>[A-Za-z0-9@._+\-]+))??(/(?P<pkgname>[A-Za-z0-9@._+\-]+))?/?$', views.listing, name='listing'),
-    url(r'^man/'
-        r'((?P<repo>[A-Za-z0-9@._+\-]+)/)??'
-        r'((?P<pkgname>[A-Za-z0-9@._+\-]+)/)?'
-        r'(?P<name_section_lang>[A-Za-z0-9@._+\-:\[\]]+?)'
-        r'(\.(?P<url_output_type>html|txt|raw))?$',
-        views.man_page, name='man_page'),
-    url(r'^search', views.search, name="search"),
-    url(r'^(?P<template_name>[a-z]+)', views.simple_view, name='simple_view'),
+    re_path(r"^$", views.index, name="index"),
+    re_path(r"^listing(/(?P<repo>[A-Za-z0-9@._+\-]+))??(/(?P<pkgname>[A-Za-z0-9@._+\-]+))?/?$", views.listing, name="listing"),
+    re_path(r"^man/"
+            r"((?P<repo>[A-Za-z0-9@._+\-]+)/)??"
+            r"((?P<pkgname>[A-Za-z0-9@._+\-]+)/)?"
+            r"(?P<name_section_lang>[A-Za-z0-9@._+\-:\[\]]+?)"
+            r"(\.(?P<url_output_type>html|txt|raw))?$",
+            views.man_page, name="man_page"),
+    re_path(r"^search", views.search, name="search"),
+    re_path(r"^(?P<template_name>[a-z]+)", views.simple_view, name="simple_view"),
 ]
